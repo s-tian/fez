b2a1427c973d25181fe6c76adf24bb0ec9512c6b
Stephen Tian <stephen@lal-99-211.ResHall.Berkeley.EDU>
2017-09-12 23:06:17 -0700

    Commit before trying to add authentication

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Movie {
+	id: number;
+	name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'My first angular app';
+  movie: Movie = {
+  	id: 1,
+  	name: 'Iron Man'
+  };
 }
