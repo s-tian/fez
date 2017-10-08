3d997b97c724e436dbd3af218751be5f887704f5
Stephen Tian <stephen.st123@gmail.com>
2017-10-07 17:41:23 -0700

    Filter by runtime

--- client/src/components/listing/searchBoxFilter.js
+++ client/src/components/listing/searchBoxFilter.js
@@ -7,14 +7,34 @@
         return filtered;
       }
       var letterMatch = new RegExp(query, 'i');
+      var timeMatch = getTimeQuery(query);
+      console.log(timeMatch);
       for (var i = 0; i < items.length; i++) {
         var item = items[i];
-        if (letterMatch.test(item.title)) {
+        if (letterMatch.test(item.title) || item.runtime <= timeMatch)  {
           filtered.push(item);
         }
       }
       return filtered;
     };
   });
+
+  function getTimeQuery(query) {
+    var reqtime = null;
+    var split_markers = [":", "hour", "h", "hr", "and"];
+    split_markers.forEach(function(element) {
+      if(query.includes(element)) {
+        components = query.split(element);
+        reqtime = 60*parseInt(components[0]);
+        if(components[1] != "") {
+          reqtime += parseInt(components[1]);
+        }
+      }
+    });
+    if(reqtime === null) {
+      return parseInt(query); // Best guess
+    }
+    return reqtime;
+  }
 
 })();
